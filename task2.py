bf7cba5c9821324cd100924cc90416f7c8f320bf
Jamison Kelly <jamison.m.kelly@gmail.com>
2025-09-15 17:49:13 -0400

    Initial black run.

--- task2.py
+++ task2.py
@@ -5,8 +5,9 @@ import random
 def create_dev_set(full_data_dir, dev_data_dir, ratio=10):
     os.makedirs(dev_data_dir, exist_ok=True)
     for file_name in sorted(os.listdir(full_data_dir)):
-        with open(f'{full_data_dir}/{file_name}') as file_full,\
-                open(f'{dev_data_dir}/{file_name}', 'w') as file_dev:
+        with open(f"{full_data_dir}/{file_name}") as file_full, open(
+            f"{dev_data_dir}/{file_name}", "w"
+        ) as file_dev:
             for line in file_full:
                 rand_num = random.randint(0, 100)
                 if rand_num < ratio:
